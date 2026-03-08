26eb108cdbfe9ce7c353f417605cbf881237c8fe
Anik <contact.anik.apps@gmail.com>
2026-03-08 09:37:14 -0700

    Switch Sphinx theme from RTD to Furo

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = ["_build"]
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = []
 
 autodoc_member_order = "bysource"
